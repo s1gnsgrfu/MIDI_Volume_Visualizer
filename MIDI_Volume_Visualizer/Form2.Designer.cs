a6faba6c70b1441a35e565770164807374c176f1
S <s.1gn.sgrfu@gmail.com>
2024-02-13 11:26:53 +0900

    Rounded corners of Form.

--- MIDI_Volume_Visualizer/Form2.Designer.cs
+++ MIDI_Volume_Visualizer/Form2.Designer.cs
@@ -61,6 +61,21 @@ namespace MIDI_Volume_Visualizer
             TransparencyKey = Color.FromArgb(255, 192, 192);
             ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
             ResumeLayout(false);
+
+            this.BackColor = Color.Black;
+            this.Opacity = 0.9;
+            int radius = 10;
+            int diameter = radius * 2;
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddPie(0, 0, diameter, diameter, 180, 90);
+            gp.AddPie(this.Width - diameter, 0, diameter, diameter, 270, 90);
+            gp.AddPie(0, this.Height - diameter, diameter, diameter, 90, 90);
+            gp.AddPie(this.Width - diameter, this.Height - diameter, diameter, diameter, 0, 90);
+            gp.AddRectangle(new Rectangle(radius, 0, this.Width - diameter, this.Height));
+            gp.AddRectangle(new Rectangle(0, radius, radius, this.Height - diameter));
+            gp.AddRectangle(new Rectangle(this.Width - radius, radius, radius, this.Height - diameter));
+
+            this.Region = new Region(gp);
         }
 
 
